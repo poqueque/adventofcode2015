f7c370b307adcb99ed01099559b1e120ba0c4867
Edi Carreras <poqueque@yahoo.es>
2020-12-09 12:12:05 +0100

    Day 09

---
+++ src/main/kotlin/days/Day09.kt
@@ -0,0 +1,38 @@
+package days
+
+class Day09 : Day(9) {
+
+    override fun partOne(): Any {
+        var string = inputString
+        repeat(40) {
+            string = lookAndSay(string)
+        }
+        return string.length
+    }
+
+    private fun lookAndSay(startString: String): String {
+        var lastChar = startString[0]
+        var counter = 0
+        val builder = StringBuilder()
+        for (i in startString.indices) {
+            if (startString[i] == lastChar) counter++
+            else {
+                builder.append(counter)
+                builder.append(lastChar)
+                counter = 1
+                lastChar = startString[i]
+            }
+        }
+        builder.append(counter)
+        builder.append(lastChar)
+        return builder.toString()
+    }
+
+    override fun partTwo(): Any {
+        var string = inputString
+        repeat(50) {
+            string = lookAndSay(string)
+        }
+        return string.length
+    }
+}
